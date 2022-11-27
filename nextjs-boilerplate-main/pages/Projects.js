433fd68fcc84403b37153d4ba8e323c28240b35f
Islam Al-Badri <mohsn12f@yahoo.com>
2022-11-27 02:55:53 +0200

    fix Projects

--- nextjs-boilerplate-main/pages/Projects.js
+++ nextjs-boilerplate-main/pages/Projects.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import React from "react";
-import styles from '../styles/Projects.module.css'
+import styles from '../styles/Projects.module.css';
 import Image from "next/image";
 
 export default function Projects(){
