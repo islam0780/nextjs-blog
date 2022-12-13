e9e3f545061a252749fa57cee473e4918a6f912c
Islam Al-Badri <mohsn12f@yahoo.com>
2022-12-13 02:08:24 +0200

    fix 5

--- nextjs-boilerplate-main/pages/posts/[id].tsx
+++ nextjs-boilerplate-main/pages/posts/[id].tsx
@@ -74,122 +74,122 @@ export async function getStaticPaths() {
     }
 }
 
-//  export default function EditPost({ post: { _id, title, content } }: ContentPageProps) {
+ export default function EditPost({ post: { _id, title, content } }: ContentPageProps) {
 
-//     const [postTitle, setPostTitle] = useState(title);
-//     const [postContent, setPostContent] = useState(content);
-//     const [error, setError] = useState('');
-//     const [message, setMessage] = useState('');
+    const [postTitle, setPostTitle] = useState(title);
+    const [postContent, setPostContent] = useState(content);
+    const [error, setError] = useState('');
+    const [message, setMessage] = useState('');
 
-//     const handleSubmit = async (e: any) => {
-//         e.preventDefault();
-//         if (postTitle && postContent) {
-//             try {
-//                 let response = await fetch('http://localhost:3000/api/editPost?id=' + _id, {
-//                     method: 'POST',
-//                     body: JSON.stringify({
-//                         title: postTitle,
-//                         content: postContent
-//                     }),
-//                     headers: {
-//                         'Accept': 'application/json, text/plain, */*',
-//                         'Content-Type': 'application/json'
-//                     }
-//                 }); 
-//                 response = await response.json();
-//                 setPostTitle('');
-//                 setPostContent('');
-//                 setError('');
-//                 setMessage('Post edited successfully');
-//             } catch (errorMessage: any) {
-//                 setError(errorMessage);
-//             }
-//         } else {
-//             return setError('All fields are required')
-//         }
-//     }
+    const handleSubmit = async (e: any) => {
+        e.preventDefault();
+        if (postTitle && postContent) {
+            try {
+                let response = await fetch('https://nextjs-blog-y3g5.vercel.app/api/editPost?id=' + _id, {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        title: postTitle,
+                        content: postContent
+                    }),
+                    headers: {
+                        'Accept': 'application/json, text/plain, */*',
+                        'Content-Type': 'application/json'
+                    }
+                }); 
+                response = await response.json();
+                setPostTitle('');
+                setPostContent('');
+                setError('');
+                setMessage('Post edited successfully');
+            } catch (errorMessage: any) {
+                setError(errorMessage);
+            }
+        } else {
+            return setError('All fields are required')
+        }
+    }
 
-//     // no such post exists
-//     if (!title && !content && !id && process.browser) {
-//         return window.location.href = '/';
-//     }
+    // no such post exists
+    if (!year && !content && !id && process.browser) {
+        return window.location.href = '/';
+    }
 
-//     return (
-//         <>
-//             <form onSubmit={handleSubmit} className="form">
-//                 {
-//                     error ? (
-//                         <div className="alert-error">
-//                             {error}
-//                         </div>
-//                     ) : null
-//                 }
-//                 {
-//                     message ? (
-//                         <div className="alert-message">
-//                             {message}
-//                         </div>
-//                     ) : null
-//                 }
-//                 <div className="form-group">
-//                     <label>Title</label>
-//                     <input type="text"
-//                         placeholder="Title of the post"
-//                         onChange={e => setPostTitle(e.target.value)}
-//                         value={postTitle ? postTitle : ''}
-//                     />
-//                 </div>
-//                 <div className="form-group">
-//                     <label>Content</label>
-//                     <textarea
-//                         name="content"
-//                         placeholder="Content of the post"
-//                         value={postContent ? postContent : ''}
-//                         onChange={e => setPostContent(e.target.value)}
-//                         cols={20}
-//                         rows={8}
-//                     />
-//                 </div>
-//                 <div className="form-group">
-//                     <button type="submit" className="submit_btn">Update</button>
-//                 </div>
-//             </form>
-        //     <style jsx>
-        //         {`
-        //             .form{
-        //                 width:400px;
-        //                 margin:10px auto;
-        //             }
-        //             .form-group{
-        //                 width:100%;
-        //                 margin-bottom:10px;
-        //                 display:block;
-        //             }
-        //             .form-group label{
-        //                 display:block;
-        //                 margin-bottom:10px;
-        //             }
-        //             .form-group input[type="text"]{
-        //                 padding:10px;
-        //                 width:100%;
-        //             }
-        //             .form-group textarea{
-        //                 padding:10px;
-        //                 width:100%;
-        //             }
-        //             .alert-error{
-        //                 width:100%;
-        //                 color:red;
-        //                 margin-bottom:10px;
-        //             }
-        //             .alert-message{
-        //                 width:100%;
-        //                 color:green;
-        //                 margin-bottom:10px;
-        //             }
-        //         `
-        //         }
-        //     </style>
-        // </>
-   // )
-//}
+    return (
+        <>
+            <form onSubmit={handleSubmit} className="form">
+                {
+                    error ? (
+                        <div className="alert-error">
+                            {error}
+                        </div>
+                    ) : null
+                }
+                {
+                    message ? (
+                        <div className="alert-message">
+                            {message}
+                        </div>
+                    ) : null
+                }
+                <div className="form-group">
+                    <label>Title</label>
+                    <input type="text"
+                        placeholder="Title of the post"
+                        onChange={e => setPostTitle(e.target.value)}
+                        value={postTitle ? postTitle : ''}
+                    />
+                </div>
+                <div className="form-group">
+                    <label>Content</label>
+                    <textarea
+                        name="content"
+                        placeholder="Content of the post"
+                        value={postContent ? postContent : ''}
+                        onChange={e => setPostContent(e.target.value)}
+                        cols={20}
+                        rows={8}
+                    />
+                </div>
+                <div className="form-group">
+                    <button type="submit" className="submit_btn">Update</button>
+                </div>
+            </form>
+            <style jsx>
+                {`
+                    .form{
+                        width:400px;
+                        margin:10px auto;
+                    }
+                    .form-group{
+                        width:100%;
+                        margin-bottom:10px;
+                        display:block;
+                    }
+                    .form-group label{
+                        display:block;
+                        margin-bottom:10px;
+                    }
+                    .form-group input[type="text"]{
+                        padding:10px;
+                        width:100%;
+                    }
+                    .form-group textarea{
+                        padding:10px;
+                        width:100%;
+                    }
+                    .alert-error{
+                        width:100%;
+                        color:red;
+                        margin-bottom:10px;
+                    }
+                    .alert-message{
+                        width:100%;
+                        color:green;
+                        margin-bottom:10px;
+                    }
+                `
+                }
+            </style>
+        </>
+   )
+}
--- nextjs-boilerplate-main/pages/posts/index.tsx
+++ nextjs-boilerplate-main/pages/posts/index.tsx
@@ -105,3 +105,4 @@ export default function Posts(props: Props) {
     </>
   );
 }
+
